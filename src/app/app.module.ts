a1289e01b8e1e12a8f489be0cdfa2cea41c6a300
Lars <lal@jyskebank.dk>
2020-10-09 13:58:36 +0200

    added prettier.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KirbyModule } from '@kirbydesign/designsystem';
-import {HomeModule} from './home/home.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import {KirbyModule} from '@kirbydesign/designsystem';
+import { KirbyModule } from '@kirbydesign/designsystem';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    KirbyModule
-  ]
+  imports: [CommonModule, KirbyModule],
 })
-export class HomeModule { }
+export class HomeModule {}
